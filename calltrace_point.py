2a8e1f9cf1e28cb5324d49fd06d43d1130df3ca6
Angelos Oikonomopoulos <a.oikonomopoulos@vu.nl>
2019-02-18 21:13:54 +0100

    Link siblings in the trace tree
    Somewhat easier to make sense of the output.

--- calltrace_point.py
+++ calltrace_point.py
@@ -1,54 +1,8 @@
 import gdb
+import os
+from importlib.machinery import SourceFileLoader
 
-class TraceTree:
-    def __init__(self):
-        self.root = TraceTreeNode("")
-    def add_trace(self, trace):
-        curr_node = self.root
-        for o in trace:
-            child = curr_node.child(o)
-            if child is None:
-                child = TraceTreeNode(o)
-                curr_node.add_child(child)
-            else:
-                child.hit()
-            curr_node = child
-            continue
-    def to_s(self):
-        if self.root is None:
-            return "<empty>"
-        return self.root.to_s("", -1)
-
-class TraceTreeNode:
-    def __init__(self, o):
-        self.obj = o
-        self.children = []
-        self.hits = 1
-    def add_child(self, o):
-        if self.child(o) is not None: # we depend on an appropriate o.__eq__
-            raise "BUG: tried to add duplicate child"
-        self.children.append(o)
-    def child(self, o):
-        for ch in self.children:
-            if o == ch.obj:
-                return ch
-    def hit(self):
-        self.hits += 1
-    def hits(self):
-        return self.hits
-    def to_s(self, acc, depth):
-        # Negative depth means "skip this many levels". Used to
-        # skip our dummy root node.
-        if depth >= 0:
-            acc += " " * depth + str(self.obj)
-            if len(self.children) == 0:
-                acc += " (#%d hits)\n" % self.hits
-                return acc
-            acc += "\n"
-        for ch in self.children:
-            acc = ch.to_s(acc, depth + 1)
-        return acc
-
+tracetree = SourceFileLoader("tracetree", os.path.dirname(os.path.abspath(__file__)) + "/tracetree.py").load_module()
 
 def calltrace_str(ct):
     acc = ""
@@ -59,7 +13,7 @@ def calltrace_str(ct):
 class CalltracePoint(gdb.Breakpoint):
     def __init__(self, spec):
         self.spec = spec
-        self.calltraces = TraceTree()
+        self.calltraces = tracetree.TraceTree()
         super(CalltracePoint, self).__init__(spec, gdb.BP_BREAKPOINT)
     def stop(self):
         calltrace = []
@@ -83,7 +37,7 @@ class CalltracePoint(gdb.Breakpoint):
         print("Call traces at %s" % self.spec)
         print(self.calltraces.to_s())
     def reset(self):
-        self.calltraces = TraceTree()
+        self.calltraces = tracetree.TraceTree()
 
 def exit_handler(bp, event):
     bp.print_summary()
---
+++ tracetree.py
@@ -0,0 +1,92 @@
+class TraceTree:
+    def __init__(self):
+        self.root = TraceTreeNode("")
+    def add_trace(self, trace):
+        curr_node = self.root
+        for o in trace:
+            child = curr_node.child(o)
+            if child is None:
+                child = TraceTreeNode(o)
+                curr_node.add_child(child)
+            else:
+                child.hit()
+            curr_node = child
+            continue
+    def to_s(self):
+        if self.root is None:
+            return "<empty>"
+        return self.root.to_s("", [], 0, True, False)
+
+def ttt(branches, depth, only_sibling):
+    # Branches are the points at which there is an "open"
+    # branching point, i.e. the points where we need to
+    # print out a '|' symbol
+    branches = [br for br in branches if br > 0]
+    acc = ""
+
+    # We walk the branch points by increasing this index
+    bridx = 0
+
+    # Walk the columns from zero to our current depth
+    for i in range(0, depth):
+        if bridx == len(branches):
+            # no pending branches
+            acc += ' '
+        elif i == branches[bridx]:
+            # We're at a branching point
+            if i == (depth - 1):
+                if not only_sibling:
+                    acc += '+'
+                else:
+                    acc += ' '
+            else:
+                acc += '|'
+            bridx += 1
+        else:
+            acc += ' '
+    return acc
+
+class TraceTreeNode:
+    def __init__(self, o):
+        self.obj = o
+        self.children = []
+        self.hits = 1
+    def add_child(self, o):
+        if self.child(o) is not None: # we depend on an appropriate o.__eq__
+            raise "BUG: tried to add duplicate child"
+        self.children.append(o)
+    def child(self, o):
+        for ch in self.children:
+            if o == ch.obj:
+                return ch
+    def hit(self):
+        self.hits += 1
+    def hits(self):
+        return self.hits
+    def to_s(self, acc, branches, depth, only_sibling, last_sibling):
+        x = ttt(branches, depth, only_sibling) + str(self.obj)
+        acc += x
+        if len(self.children) == 0:
+            acc += " (#%d hits)\n" % self.hits
+            return acc
+        acc += "\n"
+        if last_sibling:
+            branches.pop()
+        # Each lower level should have us as a branching point,
+        # except for the last child
+        branches = branches.copy()
+        # NOTE: the branch marks need to appear at (depth - 1)
+        branches.append(depth)
+        only_sibling = len(self.children) == 1
+        for (ch_off, ch) in enumerate(self.children):
+            last_sibling = ch_off == (len(self.children) - 1)
+            acc = ch.to_s(acc, branches, depth + 1, only_sibling, last_sibling)
+        return acc
+
+if __name__ == "__main__":
+    tt = TraceTree()
+
+    tt.add_trace(["a", "b", "c", "d", "g", "x", "y"])
+    tt.add_trace(["a", "b", "c", "d", "g", "w", "z"])
+    tt.add_trace(["a", "b", "e", "f"])
+    print(tt.to_s())
